92bbe121b3a9a43ecc52dc9d6af57657b44039cb
Jurayevkh <jurayevkh@gmail.com>
2024-01-08 23:28:24 +0500

    paths of components

--- angular-journey/src/app/app.routes.ts
+++ angular-journey/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { HomeComponentComponent } from './home-component/home-component.component';
+import { AboutComponentComponent } from './about-component/about-component.component';
+import { BlogComponentComponent } from './blog-component/blog-component.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:"home",component:HomeComponentComponent},
+    {path:"about",component:AboutComponentComponent},
+    {path:"blog",component:BlogComponentComponent},
+    {path:"",redirectTo:"home",pathMatch:"full"},
+    {path:"**",component:NotFoundComponent}
+];
